a8c73d5f671cf0afd4c89ae39812f071ca308673
Gustavo Silvera <grs5382@gmail.com>
2022-03-23 15:42:08 -0400

    moving more implementation to cpp

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/DReyeVRData.h
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/DReyeVRData.h
@@ -11,9 +11,6 @@
 #include <string>
 #include <unordered_map>
 
-/// NOTE: all functions here are inline to avoid nasty linker errors. Though this can
-// probably be refactored to have a proper associated .cpp file
-
 namespace DReyeVR
 {
 struct EyeData
@@ -21,47 +18,17 @@ struct EyeData
     FVector GazeDir = FVector::ZeroVector;
     FVector GazeOrigin = FVector::ZeroVector;
     bool GazeValid = false;
-    void Read(std::ifstream &InFile)
-    {
-        ReadFVector(InFile, GazeDir);
-        ReadFVector(InFile, GazeOrigin);
-        ReadValue<bool>(InFile, GazeValid);
-    }
-    void Write(std::ofstream &OutFile) const
-    {
-        WriteFVector(OutFile, GazeDir);
-        WriteFVector(OutFile, GazeOrigin);
-        WriteValue<bool>(OutFile, GazeValid);
-    }
-    FString ToString() const
-    {
-        FString Print;
-        Print += FString::Printf(TEXT("GazeDir:%s,"), *GazeDir.ToString());
-        Print += FString::Printf(TEXT("GazeOrigin:%s,"), *GazeOrigin.ToString());
-        Print += FString::Printf(TEXT("GazeValid:%d,"), GazeValid);
-        return Print;
-    }
+    void Read(std::ifstream &InFile);
+    void Write(std::ofstream &OutFile) const;
+    FString ToString() const;
 };
 
 struct CombinedEyeData : EyeData
 {
     float Vergence = 0.f; // in cm (default UE4 units)
-    void Read(std::ifstream &InFile)
-    {
-        EyeData::Read(InFile);
-        ReadValue<float>(InFile, Vergence);
-    }
-    void Write(std::ofstream &OutFile) const
-    {
-        EyeData::Write(OutFile);
-        WriteValue<float>(OutFile, Vergence);
-    }
-    FString ToString() const
-    {
-        FString Print = EyeData::ToString();
-        Print += FString::Printf(TEXT("GazeVergence:%.4f,"), Vergence);
-        return Print;
-    }
+    void Read(std::ifstream &InFile);
+    void Write(std::ofstream &OutFile) const;
+    FString ToString() const;
 };
 
 struct SingleEyeData : EyeData
@@ -72,34 +39,9 @@ struct SingleEyeData : EyeData
     FVector2D PupilPosition = FVector2D::ZeroVector;
     bool PupilPositionValid = false;
 
-    void Read(std::ifstream &InFile)
-    {
-        EyeData::Read(InFile);
-        ReadValue<float>(InFile, EyeOpenness);
-        ReadValue<bool>(InFile, EyeOpennessValid);
-        ReadValue<float>(InFile, PupilDiameter);
-        ReadFVector2D(InFile, PupilPosition);
-        ReadValue<bool>(InFile, PupilPositionValid);
-    }
-    void Write(std::ofstream &OutFile) const
-    {
-        EyeData::Write(OutFile);
-        WriteValue<float>(OutFile, EyeOpenness);
-        WriteValue<bool>(OutFile, EyeOpennessValid);
-        WriteValue<float>(OutFile, PupilDiameter);
-        WriteFVector2D(OutFile, PupilPosition);
-        WriteValue<bool>(OutFile, PupilPositionValid);
-    }
-    FString ToString() const
-    {
-        FString Print = EyeData::ToString();
-        Print += FString::Printf(TEXT("EyeOpenness:%.4f,"), EyeOpenness);
-        Print += FString::Printf(TEXT("EyeOpennessValid:%d,"), EyeOpennessValid);
-        Print += FString::Printf(TEXT("PupilDiameter:%.4f,"), PupilDiameter);
-        Print += FString::Printf(TEXT("PupilPosition:%s,"), *PupilPosition.ToString());
-        Print += FString::Printf(TEXT("PupilPositionValid:%d,"), PupilPositionValid);
-        return Print;
-    }
+    void Read(std::ifstream &InFile);
+    void Write(std::ofstream &OutFile) const;
+    FString ToString() const;
 };
 
 struct EgoVariables
@@ -115,38 +57,9 @@ struct EgoVariables
     FRotator CameraRotationAbs = FRotator::ZeroRotator;
     // Ego variables
     float Velocity = 0.f; // note this is in cm/s (default UE4 units)
-    void Read(std::ifstream &InFile)
-    {
-        ReadFVector(InFile, CameraLocation);
-        ReadFRotator(InFile, CameraRotation);
-        ReadFVector(InFile, CameraLocationAbs);
-        ReadFRotator(InFile, CameraRotationAbs);
-        ReadFVector(InFile, VehicleLocation);
-        ReadFRotator(InFile, VehicleRotation);
-        ReadValue<float>(InFile, Velocity);
-    }
-    void Write(std::ofstream &OutFile) const
-    {
-        WriteFVector(OutFile, CameraLocation);
-        WriteFRotator(OutFile, CameraRotation);
-        WriteFVector(OutFile, CameraLocationAbs);
-        WriteFRotator(OutFile, CameraRotationAbs);
-        WriteFVector(OutFile, VehicleLocation);
-        WriteFRotator(OutFile, VehicleRotation);
-        WriteValue<float>(OutFile, Velocity);
-    }
-    FString ToString() const
-    {
-        FString Print;
-        Print += FString::Printf(TEXT("VehicleLoc:%s,"), *VehicleLocation.ToString());
-        Print += FString::Printf(TEXT("VehicleRot:%s,"), *VehicleRotation.ToString());
-        Print += FString::Printf(TEXT("VehicleVel:%.4f,"), Velocity);
-        Print += FString::Printf(TEXT("CameraLoc:%s,"), *CameraLocation.ToString());
-        Print += FString::Printf(TEXT("CameraRot:%s,"), *CameraRotation.ToString());
-        Print += FString::Printf(TEXT("CameraLocAbs:%s,"), *CameraLocationAbs.ToString());
-        Print += FString::Printf(TEXT("CameraRotAbs:%s,"), *CameraRotationAbs.ToString());
-        return Print;
-    }
+    void Read(std::ifstream &InFile);
+    void Write(std::ofstream &OutFile) const;
+    FString ToString() const;
 };
 
 struct UserInputs
@@ -161,38 +74,9 @@ struct UserInputs
     bool HoldHandbrake = false;
     // Add more inputs here!
 
-    void Read(std::ifstream &InFile)
-    {
-        ReadValue<float>(InFile, Throttle);
-        ReadValue<float>(InFile, Steering);
-        ReadValue<float>(InFile, Brake);
-        ReadValue<bool>(InFile, ToggledReverse);
-        ReadValue<bool>(InFile, TurnSignalLeft);
-        ReadValue<bool>(InFile, TurnSignalRight);
-        ReadValue<bool>(InFile, HoldHandbrake);
-    }
-    void Write(std::ofstream &OutFile) const
-    {
-        WriteValue<float>(OutFile, Throttle);
-        WriteValue<float>(OutFile, Steering);
-        WriteValue<float>(OutFile, Brake);
-        WriteValue<bool>(OutFile, ToggledReverse);
-        WriteValue<bool>(OutFile, TurnSignalLeft);
-        WriteValue<bool>(OutFile, TurnSignalRight);
-        WriteValue<bool>(OutFile, HoldHandbrake);
-    }
-    FString ToString() const
-    {
-        FString Print;
-        Print += FString::Printf(TEXT("Throttle:%.4f,"), Throttle);
-        Print += FString::Printf(TEXT("Steering:%.4f,"), Steering);
-        Print += FString::Printf(TEXT("Brake:%.4f,"), Brake);
-        Print += FString::Printf(TEXT("ToggledReverse:%d,"), ToggledReverse);
-        Print += FString::Printf(TEXT("TurnSignalLeft:%d,"), TurnSignalLeft);
-        Print += FString::Printf(TEXT("TurnSignalRight:%d,"), TurnSignalRight);
-        Print += FString::Printf(TEXT("HoldHandbrake:%d,"), HoldHandbrake);
-        return Print;
-    }
+    void Read(std::ifstream &InFile);
+    void Write(std::ofstream &OutFile) const;
+    FString ToString() const;
 };
 
 struct FocusInfo
@@ -205,32 +89,9 @@ struct FocusInfo
     float Distance;
     bool bDidHit;
 
-    void Read(std::ifstream &InFile)
-    {
-        ReadFString(InFile, ActorNameTag);
-        ReadValue<bool>(InFile, bDidHit);
-        ReadFVector(InFile, HitPoint);
-        ReadFVector(InFile, Normal);
-        ReadValue<float>(InFile, Distance);
-    }
-    void Write(std::ofstream &OutFile) const
-    {
-        WriteFString(OutFile, ActorNameTag);
-        WriteValue<bool>(OutFile, bDidHit);
-        WriteFVector(OutFile, HitPoint);
-        WriteFVector(OutFile, Normal);
-        WriteValue<float>(OutFile, Distance);
-    }
-    FString ToString() const
-    {
-        FString Print;
-        Print += FString::Printf(TEXT("Hit:%d,"), bDidHit);
-        Print += FString::Printf(TEXT("Distance:%.4f,"), Distance);
-        Print += FString::Printf(TEXT("HitPoint:%s,"), *HitPoint.ToString());
-        Print += FString::Printf(TEXT("HitNormal:%s,"), *Normal.ToString());
-        Print += FString::Printf(TEXT("ActorName:%s,"), *ActorNameTag);
-        return Print;
-    }
+    void Read(std::ifstream &InFile);
+    void Write(std::ofstream &OutFile) const;
+    FString ToString() const;
 };
 
 struct EyeTracker
@@ -241,35 +102,9 @@ struct EyeTracker
     SingleEyeData Left;
     SingleEyeData Right;
     FVector2D ProjectedCoords;
-    void Read(std::ifstream &InFile)
-    {
-        ReadValue<int64_t>(InFile, TimestampDevice);
-        ReadValue<int64_t>(InFile, FrameSequence);
-        Combined.Read(InFile);
-        Left.Read(InFile);
-        Right.Read(InFile);
-        ReadFVector2D(InFile, ProjectedCoords);
-    }
-    void Write(std::ofstream &OutFile) const
-    {
-        WriteValue<int64_t>(OutFile, TimestampDevice);
-        WriteValue<int64_t>(OutFile, FrameSequence);
-        Combined.Write(OutFile);
-        Left.Write(OutFile);
-        Right.Write(OutFile);
-        WriteFVector2D(OutFile, ProjectedCoords);
-    }
-    FString ToString() const
-    {
-        FString Print;
-        Print += FString::Printf(TEXT("TimestampDevice:%ld,"), long(TimestampDevice));
-        Print += FString::Printf(TEXT("FrameSequence:%ld,"), long(FrameSequence));
-        Print += FString::Printf(TEXT("COMBINED:{%s},"), *Combined.ToString());
-        Print += FString::Printf(TEXT("LEFT:{%s},"), *Left.ToString());
-        Print += FString::Printf(TEXT("RIGHT:{%s},"), *Right.ToString());
-        Print += FString::Printf(TEXT("ReticleCoords:%s,"), *ProjectedCoords.ToString());
-        return Print;
-    }
+    void Read(std::ifstream &InFile);
+    void Write(std::ofstream &OutFile) const;
+    FString ToString() const;
 };
 
 enum class Gaze
