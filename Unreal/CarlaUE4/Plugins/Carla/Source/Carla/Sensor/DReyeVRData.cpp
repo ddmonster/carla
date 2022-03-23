59ee5e49d3a117db8c260a23fb8f821f82ee4333
Gustavo Silvera <grs5382@gmail.com>
2022-03-23 14:56:13 -0400

    moving the source for CustomActor to cpp

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/DReyeVRData.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/DReyeVRData.cpp
@@ -3,4 +3,44 @@
 #include <string>
 #include <unordered_map>
 
-// std::unordered_map<std::string, class DReyeVR::CustomActorData> *DReyeVR::CustomActorData::AllCustomActors = nullptr;
+namespace DReyeVR
+{
+
+void CustomActorData::Read(std::ifstream &InFile)
+{
+    ReadValue<char>(InFile, TypeId);
+    ReadFVector(InFile, Location);
+    ReadFRotator(InFile, Rotation);
+    ReadFVector(InFile, Scale3D);
+    ReadFString(InFile, Other);
+    ReadFString(InFile, Name);
+}
+
+void CustomActorData::Write(std::ofstream &OutFile) const
+{
+    WriteValue<char>(OutFile, static_cast<char>(TypeId));
+    WriteFVector(OutFile, Location);
+    WriteFRotator(OutFile, Rotation);
+    WriteFVector(OutFile, Scale3D);
+    WriteFString(OutFile, Other);
+    WriteFString(OutFile, Name);
+}
+
+FString CustomActorData::ToString() const
+{
+    FString Print = "";
+    Print += FString::Printf(TEXT("Type:%d,"), int(TypeId));
+    Print += FString::Printf(TEXT("Name:%s,"), *Name);
+    Print += FString::Printf(TEXT("Location:%s,"), *Location.ToString());
+    Print += FString::Printf(TEXT("Rotation:%s,"), *Rotation.ToString());
+    Print += FString::Printf(TEXT("Scale3D:%s,"), *Scale3D.ToString());
+    Print += FString::Printf(TEXT("Other:%s,"), *Other);
+    return Print;
+}
+
+std::string CustomActorData::GetUniqueName() const
+{
+    return TCHAR_TO_UTF8(*Name);
+}
+
+}; // namespace DReyeVR
--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/DReyeVRData.h
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/DReyeVRData.h
@@ -552,42 +552,10 @@ class CustomActorData
 
     CustomActorData() = default;
 
-    void Read(std::ifstream &InFile)
-    {
-        ReadValue<char>(InFile, TypeId);
-        ReadFVector(InFile, Location);
-        ReadFRotator(InFile, Rotation);
-        ReadFVector(InFile, Scale3D);
-        ReadFString(InFile, Other);
-        ReadFString(InFile, Name);
-    }
-
-    void Write(std::ofstream &OutFile) const
-    {
-        WriteValue<char>(OutFile, static_cast<char>(TypeId));
-        WriteFVector(OutFile, Location);
-        WriteFRotator(OutFile, Rotation);
-        WriteFVector(OutFile, Scale3D);
-        WriteFString(OutFile, Other);
-        WriteFString(OutFile, Name);
-    }
-
-    FString ToString() const
-    {
-        FString Print = "";
-        Print += FString::Printf(TEXT("Type:%d,"), int(TypeId));
-        Print += FString::Printf(TEXT("Name:%s,"), *Name);
-        Print += FString::Printf(TEXT("Location:%s,"), *Location.ToString());
-        Print += FString::Printf(TEXT("Rotation:%s,"), *Rotation.ToString());
-        Print += FString::Printf(TEXT("Scale3D:%s,"), *Scale3D.ToString());
-        Print += FString::Printf(TEXT("Other:%s,"), *Other);
-        return Print;
-    }
-
-    std::string GetUniqueName() const
-    {
-        return TCHAR_TO_UTF8(*Name);
-    }
+    void Read(std::ifstream &InFile);
+    void Write(std::ofstream &OutFile) const;
+    FString ToString() const;
+    std::string GetUniqueName() const;
 };
 
 }; // namespace DReyeVR
